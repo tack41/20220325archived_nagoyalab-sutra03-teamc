64a5cc4494a4eeb2fa5ecf2e01bc09fa7bde82c4
Takashi Tsuzuki <tack41@gmail.com>
2018-11-12 21:49:18 +0900

    Androidアプリの画面1(ガワのみ)作成

---
+++ AndroidStudio/MimamoruKun/app/src/test/java/com/slack/nagoyalab_sutra03/teamc/mimamorukun/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.slack.nagoyalab_sutra03.teamc.mimamorukun;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
